b0c2ef771b187b40f6f5bd1fff0cdff6a53973d5
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-02-13 12:00:21 +0200

    import: ReactTable

--- src/containers/Leaderboards/Leaderboards.js
+++ src/containers/Leaderboards/Leaderboards.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import users from '../../placeholders/users.json'; 
 
 const Leaderboards = () => {
   return (
