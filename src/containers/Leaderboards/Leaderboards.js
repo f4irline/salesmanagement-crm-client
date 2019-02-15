67b4176fd436688ee54c516aecff37c1754dcf47
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-02-15 16:18:08 +0200

    Add: basic React-Table for placeholders

--- src/containers/Leaderboards/Leaderboards.js
+++ src/containers/Leaderboards/Leaderboards.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import { Component } from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import users from '../../placeholders/users.json'; 
+import './Leaderboards.css';
 
-const Leaderboards = () => {
-  return (
-    <div className='content'>
-      <p>Leaderboard</p>
-    </div>
-  );
-};
+class Leaderboards extends Component {
+  
+  render() {
+    const data = users.users;
+    const columns = [{
+      Header: 'Name',
+      accessor: 'user_name'
+    },{
+      Header: 'Role',
+      accessor: 'role'
+    },{
+      Header: 'User ID',
+      accessor: 'user_id'
+    },{
+      Header: 'Last Login',
+      accessor: 'last_login'
+    }];
+
+    return (
+      <div className="content" id="table">
+        <ReactTable
+          data={data}
+          columns={columns}
+          defaultPageSize = {10}
+          pageSizeOptions = {[10, 20]}
+        />
+      </div>      
+    );
+
+  }
+}
 
 export default Leaderboards;
