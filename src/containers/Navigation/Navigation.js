632a4f82858b7028bebba343103078b0d1ce235e
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-02-18 20:39:37 +0200

    add icon to dashboard and leadboard

--- src/containers/Navigation/Navigation.js
+++ src/containers/Navigation/Navigation.js
@@ -6,17 +6,19 @@ import ActionItem from '../../components/ActionItem/ActionItem';
 
 import Settings from '@material-ui/icons/Settings';
 import ExitToApp from '@material-ui/icons/ExitToApp';
+import Dashboard from '@material-ui/icons/Dashboard';
+import TableChart from '@material-ui/icons/TableChart';
 
 const Navigation = (props) => {
   return (
     <div className='Navigation'>
       <div className='navigation-wrapper'>
-        <NavItem url='/'>Dashboard</NavItem>
-        <NavItem url='/leaderboards'>Leaderboards</NavItem>
+        <NavItem url='/'>{<Dashboard/>}Dashboard</NavItem>
+        <NavItem url='/leaderboards'>{<TableChart/>}Leaderboards</NavItem>
       </div>
       <div className='action-wrapper'>
-        <ActionItem name = 'configuration' icon = {<Settings/>} onClick = {props.handleConfiguration}/>
-        <ActionItem name = 'logout' icon = {<ExitToApp/>} onClick = {props.handleLogout}/>
+        <ActionItem name = 'Configuration' icon = {<Settings/>} onClick = {props.handleConfiguration}/>
+        <ActionItem name = 'Logout' icon = {<ExitToApp/>} onClick = {props.handleLogout}/>
       </div>
     </div>
   );
