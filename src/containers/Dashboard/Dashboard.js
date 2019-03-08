1e6334ee42c89ac8a3816269dcb32daf9c32737e
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-08 12:48:59 +0200

    Dashboard class Component created

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Component } from 'react';
 import './Dashboard.css';
 
-const Dashboard = () => {
-  return (
-    <div className='Dashboard'>
-    </div>
-  );
-};
+class Dashboard extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('Dashboard constructor');
+  }
+
+  render() {
+    console.log('Dashboard render');
+    return (
+      <div className='Dashboard'>
+      hellurei
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
