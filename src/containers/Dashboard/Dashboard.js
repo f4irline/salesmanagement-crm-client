277587414a59ea4813c89051a6db473ec72176a2
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-11 11:18:47 +0200

    refactor to separate UserData component BASE_URL changed to heroku

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -2,31 +2,20 @@ import React from 'react';
 import { Component } from 'react';
 import './Dashboard.css';
 
+import UserData from './UserData/UserData';
+
 class Dashboard extends Component {
-  BASE_URL = "http://localhost:8080/user/";
+  BASE_URL = "https://vc-system-server.herokuapp.com/user/";
   constructor(props) {
     super(props);
     console.log('Dashboard constructor');
-    let user = {};
-    this.state = {name: props.name, user: user};
-  }
-
-  componentDidMount() {
-    let url = `${this.BASE_URL}${this.state.name}`;
-    console.log(url);
-    fetch(url).then(
-      (resp) => resp.json()).then((user) => {
-      console.log(user);
-      let stateObj = {user: user};
-      this.setState(stateObj);
-    }).catch((e) => console.log(e));
   }
 
   render() {
     console.log('Dashboard render');
     return (
       <div className='Dashboard'>
-      hellurei {this.state.name}{this.state.user.name}
+        <UserData name={this.props.name}/>
       </div>
     );
   }
