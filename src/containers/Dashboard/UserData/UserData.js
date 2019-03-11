b4c4a9aa7e203d863e628db354f0c330d4741e04
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-11 14:16:44 +0200

    add paper userData

--- src/containers/Dashboard/UserData/UserData.js
+++ src/containers/Dashboard/UserData/UserData.js
@@ -3,12 +3,15 @@ import { Component } from 'react';
 import './UserData.css';
 
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
 class UserData extends Component {
   constructor(props) {
     super(props);
     console.log('UserData constructor');
-    this.state = {user: props.user};
+
+    console.log(props.name);
+    this.state = {user: props.user, name: props.name};
   }
 
   render() {
@@ -16,11 +19,16 @@ class UserData extends Component {
     return (
       <div className='UserData'>
         <Typography variant="h2">
-            hellurei {this.state.user.name}
+          hellurei {this.state.user.name}
         </Typography>
         <Typography variant="h3" gutterBottom>
             lastLogin: {this.state.user.lastLogin}
         </Typography>
+        <Paper>
+          <Typography variant="h5">
+            UserName: {this.state.user.name} 
+          </Typography>
+        </Paper>
       </div>
     );
   }
