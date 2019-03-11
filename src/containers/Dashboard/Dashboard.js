8fbc98eaa7a6067368f51444eeae1e4b5f34f719
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-11 14:15:12 +0200

    add loading state

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -6,17 +6,14 @@ import UserData from './UserData/UserData';
 import UserGraph from './UserGraph/UserGraph';
 import CompanyGraph from './CompanyGraph/CompanyGraph';
 
-import userdata from '../../placeholders/user.json';
-
 class Dashboard extends Component {
   BASE_URL = 'https://vc-system-server.herokuapp.com/user/';
   constructor(props) {
     super(props);
     console.log('Dashboard constructor');
-    console.log(userdata.hit_rate);
     
     let user = {};
-    this.state = {name: props.name, user: user};
+    this.state = {name: props.name, user: user, loading: true};
   }
   
   componentDidMount() {
@@ -25,17 +22,27 @@ class Dashboard extends Component {
     fetch(url).then(
       (resp) => resp.json()).then((user) => {
       console.log(user);
-      let stateObj = {user: user};
-      this.setState(stateObj);
+      this.setState({user: user}, () => {
+        this.setState({loading: false});
+      });
     }).catch((e) => console.log(e));
   }
 
   render() {
-    console.log('Dashboard render');
+    console.log('Dashboard render ');
+
+    if (this.state.loading) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )
+    }
+
     return (
       <div className='Dashboard'>
         <div className='user-wrapper'>
-          <UserData user={this.state.user} />
+          <UserData user={this.state.user}/>
           <UserGraph />
         </div>
         <div className='company-wrapper'>
