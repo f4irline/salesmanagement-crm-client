db2c79b1370f14c852304247c89aec6b02cf696f
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-11 12:19:24 +0200

    add UserGraph and CompanyGraph componets

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -3,19 +3,30 @@ import { Component } from 'react';
 import './Dashboard.css';
 
 import UserData from './UserData/UserData';
+import UserGraph from './UserGraph/UserGraph';
+import CompanyGraph from './CompanyGraph/CompanyGraph';
+
+import userdata from '../../placeholders/user.json';
 
 class Dashboard extends Component {
-  BASE_URL = "https://vc-system-server.herokuapp.com/user/";
+  BASE_URL = 'https://vc-system-server.herokuapp.com/user/';
   constructor(props) {
     super(props);
     console.log('Dashboard constructor');
+    console.log(userdata.hit_rate);
   }
 
   render() {
     console.log('Dashboard render');
     return (
       <div className='Dashboard'>
-        <UserData name={this.props.name}/>
+        <div className='user-wrapper'>
+          <UserData name={this.props.name}/>
+          <UserGraph />
+        </div>
+        <div className='company-wrapper'>
+          <CompanyGraph />
+        </div>
       </div>
     );
   }
