2b91c4ed25d7b051d327a1d6594419e9daf3db56
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-14 12:55:07 +0200

    Add grid system for userdata

--- src/containers/Dashboard/UserData/UserData.js
+++ src/containers/Dashboard/UserData/UserData.js
@@ -5,7 +5,29 @@ import './UserData.css';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import {withStyles} from '@material-ui/core/styles';
 import { List, ListItemText, ListItem } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
+
+const styles = {
+  listItem: {
+    padding: '0.5vh 1vw'
+  },
+  paper: {
+    margin: '1vw',
+    padding: '1vw'
+  },
+  header: {
+    margin: '1vh 0.5vw 0 0.5vw',
+    padding: '0 0.5vw',
+    textTransform: 'uppercase',
+    fontWeight: 800
+  },
+  login: {
+    margin: '0 1vw',
+    padding: '0 1vw'
+  }
+};
 
 class UserData extends Component {
   constructor(props) {
@@ -16,41 +38,91 @@ class UserData extends Component {
   }
 
   render() {
+
+    const { classes } = this.props;
+
     console.log('UserData render');
     return (
       <Grid item xs={12} lg={6} className='UserData'>
-        <Typography variant="h2">
+        <Typography variant='h2' classes={{root: classes.header}}>
           Hello, {this.state.user.name}
         </Typography>
-        <Typography variant="h5" gutterBottom>
-            your last login: {this.state.user.lastLogin}
+        <Typography variant="h5" classes={{root: classes.login}}>
+          Your last login: {this.state.user.lastLogin}
         </Typography>
-        <Paper>
+        <Paper elevation={5} classes={{root: classes.paper}}>
           <div>
             <List>
-              <ListItem>
-                <ListItemText primary={`Hitrate: ${this.state.userData.hit_rate}`}></ListItemText>
+              <ListItem classes={{root: classes.listItem}}>
+                <Grid item xs={6}>
+                  <ListItemText primary={'Hitrate:'}></ListItemText>
+                </Grid>
+                <Grid item xs={6}>
+                  <ListItemText primary={this.state.userData.hit_rate}></ListItemText>
+                </Grid>
               </ListItem>
-              <ListItem>
-                <ListItemText primary={`Average sales: ${this.state.userData.avg_sales}`}></ListItemText>
+              <Divider variant='middle'/>
+              <ListItem classes={{root: classes.listItem}}>
+                <Grid item xs={6}>
+                  <ListItemText primary={'Average sales:'}></ListItemText>
+                </Grid>
+                <Grid item xs={6}>
+                  <ListItemText primary={this.state.userData.avg_sales}></ListItemText>
+                </Grid>
               </ListItem>
-              <ListItem>
-                <ListItemText primary={`Total sales: ${this.state.userData.total_sales}`}></ListItemText>
+              <Divider variant='middle'/>
+              <ListItem classes={{root: classes.listItem}}>
+                <Grid item xs={6}>
+                  <ListItemText primary={'Total sales:'}></ListItemText>
+                </Grid>
+                <Grid item xs={6}>
+                  <ListItemText primary={this.state.userData.total_sales}></ListItemText>
+                </Grid>
               </ListItem>
-              <ListItem>
-                <ListItemText primary={`Contacts amount: ${this.state.userData.contacts_amount}`}></ListItemText>
+              <Divider variant='middle'/>
+              <ListItem classes={{root: classes.listItem}}>
+                <Grid item xs={6}>
+                  <ListItemText primary={'Contacts amount:'}></ListItemText>
+                </Grid>
+                <Grid item xs={6}>
+                  <ListItemText primary={this.state.userData.contacts_amount}></ListItemText>
+                </Grid>
               </ListItem>
-              <ListItem>
-                <ListItemText primary={`Meetings amount: ${this.state.userData.meetings_amount}`}></ListItemText>
+              <Divider variant='middle'/>
+              <ListItem classes={{root: classes.listItem}}>
+                <Grid item xs={6}>
+                  <ListItemText primary={'Meetings amount:'}></ListItemText>
+                </Grid>
+                <Grid item xs={6}>
+                  <ListItemText primary={this.state.userData.meetings_amount}></ListItemText>
+                </Grid>
               </ListItem>
-              <ListItem>
-                <ListItemText primary={`Offers amount: ${this.state.userData.offers_amount}`}></ListItemText>
+              <Divider variant='middle'/>
+              <ListItem classes={{root: classes.listItem}}>
+                <Grid item xs={6}>
+                  <ListItemText primary={'Offers amount:'}></ListItemText>
+                </Grid>
+                <Grid item xs={6}>
+                  <ListItemText primary={this.state.userData.offers_amount}></ListItemText>
+                </Grid>
               </ListItem>
-              <ListItem>
-                <ListItemText primary={`Deals amount: ${this.state.userData.deals_amount}`}></ListItemText>
+              <Divider variant='middle'/>
+              <ListItem classes={{root: classes.listItem}}>
+                <Grid item xs={6}>
+                  <ListItemText primary={'Deals amount:'}></ListItemText>
+                </Grid>
+                <Grid item xs={6}>
+                  <ListItemText primary={this.state.userData.deals_amount}></ListItemText>
+                </Grid>
               </ListItem>
-              <ListItem>
-                <ListItemText primary={`Goal: ${this.state.userData.goal}`}></ListItemText>
+              <Divider variant='middle'/>
+              <ListItem classes={{root: classes.listItem}}>
+                <Grid item xs={6}>
+                  <ListItemText primary={'Goal:'}></ListItemText>
+                </Grid>
+                <Grid item xs={6}>
+                  <ListItemText primary={this.state.userData.goal}></ListItemText>
+                </Grid>
               </ListItem>
             </List>
           </div>
@@ -60,4 +132,4 @@ class UserData extends Component {
   }
 }
 
-export default UserData;
+export default withStyles(styles)(UserData);
