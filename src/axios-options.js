06027318c3bdabaffb195b959115e49b6f5aae1b
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-15 19:40:39 +0200

    Add checking to axios for production build

--- src/axios-options.js
+++ src/axios-options.js
@@ -3,21 +3,22 @@ import axios from 'axios';
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-/**
- * Production instance
- * 
-const CSRF_TOKEN = document.cookie.match(new RegExp('XSRF-TOKEN=([^;]+)'))[1];
+let instance = null;
 
-const instance = axios.create({
-  baseURL: 'https://vc-system-server.herokuapp.com/',
-  headers: { 
-    'X-XSRF-TOKEN': CSRF_TOKEN 
-  }
-});
- */
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  instance = axios.create({
+    baseURL: 'http://localhost:8080'
+  });
+} else {
+  const CSRF_TOKEN = document.cookie.match(new RegExp('XSRF-TOKEN=([^;]+)'))[1];
 
-const instance = axios.create({
-  baseURL: 'http://localhost:8080'
-});
+  instance = axios.create({
+    baseURL: 'https://vc-system-server.herokuapp.com/',
+    headers: { 
+      'X-XSRF-TOKEN': CSRF_TOKEN 
+    }
+  });
+}
+
 
 export default instance;
