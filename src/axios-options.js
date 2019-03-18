5c436d53fe45cdffa8c284d596d334b1cdcb2b3f
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-18 17:52:51 +0200

    Configure the new path for REST Api requests

--- src/axios-options.js
+++ src/axios-options.js
@@ -7,13 +7,13 @@ let instance = null;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   instance = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://localhost:8080/api'
   });
 } else {
   const CSRF_TOKEN = document.cookie.match(new RegExp('XSRF-TOKEN=([^;]+)'))[1];
 
   instance = axios.create({
-    baseURL: 'https://vc-system-server.herokuapp.com/',
+    baseURL: 'https://vc-system-server.herokuapp.com/api',
     headers: { 
       'X-XSRF-TOKEN': CSRF_TOKEN 
     }
