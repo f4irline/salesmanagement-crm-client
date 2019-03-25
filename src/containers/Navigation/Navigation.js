eb8817f66ec6ab9f40033dbe1332c7e6f0a50be0
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-25 12:57:42 +0200

    change events icon

--- src/containers/Navigation/Navigation.js
+++ src/containers/Navigation/Navigation.js
@@ -8,13 +8,15 @@ import Settings from '@material-ui/icons/Settings';
 import ExitToApp from '@material-ui/icons/ExitToApp';
 import Dashboard from '@material-ui/icons/Dashboard';
 import TableChart from '@material-ui/icons/TableChart';
+import History from '@material-ui/icons/History';
+
 const Navigation = (props) => {
   return (
     <div className='Navigation'>
       <div className='navigation-wrapper'>
         <NavItem url='/' icon={<Dashboard />}>Oma sivu</NavItem>
         <NavItem url='/leaderboards' icon={<TableChart />}>Sijoitukset</NavItem>
-        <NavItem url='/events' icon={<TableChart />}>Tapahtumat</NavItem>
+        <NavItem url='/events' icon={<History />}>Tapahtumat</NavItem>
       </div>
       <div className='action-wrapper'>
         <ActionItem icon = {<Settings/>} onClick = {props.handleConfiguration}>Hallinta</ActionItem>
