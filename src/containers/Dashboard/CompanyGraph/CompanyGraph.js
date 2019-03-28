d7a31f03ffa5bd0a4fb516843a68ff63df1160b3
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-28 15:09:00 +0200

    add numbers to company graph

--- src/containers/Dashboard/CompanyGraph/CompanyGraph.js
+++ src/containers/Dashboard/CompanyGraph/CompanyGraph.js
@@ -13,20 +13,6 @@ import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
-class CustomizedAxisTick extends PureComponent {
-  render() {
-    const {
-      x, y, payload,
-    } = this.props;
-
-    return (
-      <g transform={`translate(${x},${y})`}>
-        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
-      </g>
-    );
-  }
-}
-
 class CompanyGraph extends PureComponent {
   
   state = {
@@ -130,11 +116,33 @@ class CompanyGraph extends PureComponent {
             <YAxis />
             <Tooltip />
             <Legend />
-            <Line type="monotone" dataKey="sum" stroke="#8884d8" dot={null} />
+            <Line type="monotone" dataKey="sum" stroke="#8884d8" label={<CustomizedLabel />} dot={null} />
             <Line connectNulls type="monotone" dataKey="goal" stroke="red" dot={null} />
           </LineChart>
         </div>
       </Grid>
+    );
+  }
+}
+class CustomizedLabel extends PureComponent {
+  render() {
+    const {
+      x, y, stroke, value,
+    } = this.props;
+    
+    return <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">{value}</text>;
+  }
+}
+class CustomizedAxisTick extends PureComponent {
+  render() {
+    const {
+      x, y, payload,
+    } = this.props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
+      </g>
     );
   }
 }
