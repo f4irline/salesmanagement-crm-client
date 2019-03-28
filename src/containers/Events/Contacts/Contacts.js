3c005153e2f19d813e917915715c52c57d650ec4
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-28 13:25:32 +0200

    add Contacts table

--- src/containers/Events/Contacts/Contacts.js
+++ src/containers/Events/Contacts/Contacts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
+import MUIDataTable from 'mui-datatables';
 import './Contacts.css';
 
 import {print} from '../../../utils/Debug';
@@ -13,10 +14,33 @@ class Contacts extends Component {
     this.setState({data: this.props.data});
   }
 
+  mapData(data) {
+
+    print ('Contacts', 'mapData');
+
+    let newData = [];
+    newData = data.map((object) => {
+      let rowData = [];
+      for (let data in object) {
+        if (data !== 'eventId' && data !== 'eventType' && data !== 'user' && data !== 'sum' && data !== 'place') {
+          if (data === 'lead') {
+            rowData.push(object[data].companyName);
+          } else {
+            rowData.push(object[data]);
+          }
+          
+        }
+      }
+      return rowData;
+    });
+
+    return newData;
+  }
   
   render() {
 
     print ('Contacts', 'render');
+    console.log(this.state.data);
 
     if (this.state.loading) {
       return (
@@ -26,9 +50,99 @@ class Contacts extends Component {
       );
     }
 
+    const data = this.state.data;   
+    const newData = this.mapData(data);
+    const columns = [
+      {
+        name: 'date',
+        options: {
+          filter: false,
+          sort: true,
+        }
+      },
+      {
+        name: 'companyName',
+        options: {
+          filter: false,
+          sort: true,
+        }
+      },
+      {
+        name: 'contactPerson',
+        options: {
+          filter: false,
+          sort: true,
+        }
+      },
+      {
+        name: 'phoneNumber',
+        options: {
+          filter: false,
+          sort: true,
+        }
+      },
+      {
+        name: 'email',
+        options: {
+          filter: false,
+          sort: true,
+        }
+      },
+      {
+        name: 'notes',
+        options: {
+          filter: false,
+          sort: true,
+        }
+      }
+    ];
+
+    const options = {
+      filterType: 'multiselect',
+      selectableRows: false,
+      search: false,
+      filter: false,
+      textLabels: {
+        body: {
+          noMatch: 'Ei tuloksia',
+          toolTip: 'Järjestä',
+        },
+        pagination: {
+          next: 'Seuraava sivu',
+          previous: 'Edellinen sivu',
+          rowsPerPage: 'Rivejä / sivu:',
+          displayRows: '-',
+        },
+        toolbar: {
+          search: 'Etsi',
+          downloadCsv: 'Lataa CSV',
+          print: 'Tulosta',
+          viewColumns: 'Sarakkeet',
+          filterTable: 'Suodata',
+        },
+        viewColumns: {
+          title: 'Näytetyt Sarakkeet',
+          titleAria: 'Näytä/Piilota Taulukon Sarakkeet',
+        },
+        selectedRows: {
+          text: 'rivejä valittu',
+          delete: 'Poista',
+          deleteAria: 'Poista Valitut Rivit',
+        },
+      }
+    
+    };
+
     return (
       <div className='Contacts'>
-        <p>Contacts</p>
+        <div id='table'>
+          <MUIDataTable
+            title={'Yhteydenotot'}
+            data={newData}
+            columns={columns}
+            options={options}
+          />
+        </div>  
       </div>
      
     );
