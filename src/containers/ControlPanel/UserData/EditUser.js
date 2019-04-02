1803057fe5d665f4addd401b4d076326e4cfbca6
Elias <elias.pohjalainen@tuni.fi>
2019-04-02 14:02:04 +0300

    EditUser initialize

---
+++ src/containers/ControlPanel/UserData/EditUser.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
+const EditUser = (props) => {
+  return (
+    <Grid container direction='column' alignItems='center'>
+      <Grid item xs={12} style={{textAlign: 'center'}}>
+        <Typography variant='h4' gutterBottom>
+          KÄYTTÄJÄN MUOKKAUS
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
