ffbff83d4e7df3e87415eab26e7f4378499df68b
Tommi Lepola <lepola.tommi@gmail.com>
2019-04-02 11:29:33 +0300

    Revert Contacts component to functional component

--- src/containers/Events/Contacts/Contacts.js
+++ src/containers/Events/Contacts/Contacts.js
@@ -1,20 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import MUIDataTable from 'mui-datatables';
 import './Contacts.css';
-import AlertDialog from '../../../components/AlertDialog/AlertDialog'
 
-import DeleteIcon from '@material-ui/icons/Delete';
-import CreateIcon from '@material-ui/icons/Create';
-import IconButton from '@material-ui/core/IconButton';
+const Contacts = (props) => {
 
-class Contacts extends Component {
-
-  state = {
-    showDialog: false,
-    newData: []
-  }
-
-  mapData(data) {
+  const mapData = (data) => {
     let newData = [];
     newData = data.map((object) => {
       let rowData = [];
@@ -25,144 +15,108 @@ class Contacts extends Component {
           } else {
             rowData.push(object[data]);
           }
-          
         }
       }
-      rowData.push(
-        <IconButton aria-label='Delete' onClick={this.onClickDeleteHandler.bind(this)}>
-          <DeleteIcon />
-        </IconButton>
-      )
-      rowData.push(
-        <IconButton aria-label='Create' onClick={this.onClickEditHandler.bind(this)}>
-          <CreateIcon />
-        </IconButton>
-      )
       return rowData;
     });
     return newData;
   };
 
-  componentDidMount() {
-    const data = this.props.data;   
-    this.setState({newData: this.mapData(data)})
-  }
+  const data = props.data;   
+  const newData = mapData(data);
 
-  onClickDeleteHandler() {
-    this.setState({showDialog: true});
-  }
-
-  onClickCloseHandler(name) {
-    if(name === 'delete') {
-
+  const columns = [
+    {
+      name: 'päivämäärä',
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: 'yritys',
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: 'yhteyshenkilö',
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: 'puhelinnumero',
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: 'sähköposti',
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: 'lisätiedot',
+      options: {
+        filter: false,
+        sort: true,
+      }
     }
-    this.setState({showDialog: false});
-  }
+  ];
 
-  onClickEditHandler() {
-    
-  }
-
-  render() {
-    const columns = [
-      {
-        name: 'päivämäärä',
-        options: {
-          filter: false,
-          sort: true,
-        }
+  const options = {
+    filterType: 'multiselect',
+    selectableRows: false,
+    search: false,
+    filter: false,
+    textLabels: {
+      body: {
+        noMatch: 'Ei tuloksia',
+        toolTip: 'Järjestä',
       },
-      {
-        name: 'yritys',
-        options: {
-          filter: false,
-          sort: true,
-        }
+      pagination: {
+        next: 'Seuraava sivu',
+        previous: 'Edellinen sivu',
+        rowsPerPage: 'Rivejä / sivu:',
+        displayRows: '-',
       },
-      {
-        name: 'yhteyshenkilö',
-        options: {
-          filter: false,
-          sort: true,
-        }
+      toolbar: {
+        search: 'Etsi',
+        downloadCsv: 'Lataa CSV',
+        print: 'Tulosta',
+        viewColumns: 'Sarakkeet',
+        filterTable: 'Suodata',
       },
-      {
-        name: 'puhelinnumero',
-        options: {
-          filter: false,
-          sort: true,
-        }
+      viewColumns: {
+        title: 'Näytetyt Sarakkeet',
+        titleAria: 'Näytä/Piilota Taulukon Sarakkeet',
       },
-      {
-        name: 'sähköposti',
-        options: {
-          filter: false,
-          sort: true,
-        }
+      selectedRows: {
+        text: 'rivejä valittu',
+        delete: 'Poista',
+        deleteAria: 'Poista Valitut Rivit',
       },
-      {
-        name: 'lisätiedot',
-        options: {
-          filter: false,
-          sort: true,
-        }
-      }, {
-        name: 'Poista'
-      }, {
-        name: 'Muokkaa'
-      }
-    ];
+    }
   
-    const options = {
-      filterType: 'multiselect',
-      selectableRows: false,
-      search: false,
-      filter: false,
-      textLabels: {
-        body: {
-          noMatch: 'Ei tuloksia',
-          toolTip: 'Järjestä',
-        },
-        pagination: {
-          next: 'Seuraava sivu',
-          previous: 'Edellinen sivu',
-          rowsPerPage: 'Rivejä / sivu:',
-          displayRows: '-',
-        },
-        toolbar: {
-          search: 'Etsi',
-          downloadCsv: 'Lataa CSV',
-          print: 'Tulosta',
-          viewColumns: 'Sarakkeet',
-          filterTable: 'Suodata',
-        },
-        viewColumns: {
-          title: 'Näytetyt Sarakkeet',
-          titleAria: 'Näytä/Piilota Taulukon Sarakkeet',
-        },
-        selectedRows: {
-          text: 'rivejä valittu',
-          delete: 'Poista',
-          deleteAria: 'Poista Valitut Rivit',
-        },
-      }
-    
-    };
-    return (
-      <div className='Contacts'>
-        {this.state.showDialog ? <AlertDialog title='Poista tapahtuma' description = 'Haluatko varmasti poistaa tapahtuman?' handleClose={this.onClickCloseHandler.bind(this)} /> : null}
-        <div id='table'>
-          <MUIDataTable
-            title={'Yhteydenotot'}
-            data={this.state.newData}
-            columns={columns}
-            options={options}
-          />
-        </div>  
-      </div>
-      
-    );
-  }
+  };
+  return (
+    <div className='Contacts'>
+      <div id='table'>
+        <MUIDataTable
+          title={'Yhteydenotot'}
+          data={newData}
+          columns={columns}
+          options={options}
+        />
+      </div>  
+    </div>
+  );
 };
 
 export default Contacts;
