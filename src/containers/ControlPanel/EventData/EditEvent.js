2132c86c19b0aab9c3c49bc7635bbb7790b4a686
Tommi Lepola <lepola.tommi@gmail.com>
2019-04-02 13:23:40 +0300

    Sketch EditEvent form

--- src/containers/ControlPanel/EventData/EditEvent.js
+++ src/containers/ControlPanel/EventData/EditEvent.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
 const EditEvent = (props) => {
-  console.log(props.data);
   return (
-    <div>
-      Hei
-    </div>
+    <Grid container direction='column' alignItems='center'>
+      <Grid item xs={12} style={{textAlign: 'center'}}>
+        <Typography variant='h4' gutterBottom>
+          TAPAHTUMAN MUOKKAUS
+        </Typography>
+      </Grid>
+      <Grid container>
+        <Grid item container justify='center' xs={6}>
+          <TextField
+            fullWidth
+            variant='outlined'></TextField>
+        </Grid>
+        <Grid item container justify='center' xs={6}>
+          <TextField
+            variant='outlined'></TextField>
+        </Grid>
+      </Grid>
+    </Grid>  
   );
 };
 
