00fae744cf4a830f40eed19d85e9b86f17946c8d
Tommi Lepola <lepola.tommi@gmail.com>
2019-08-22 18:37:41 +0300

    Remove CSRF token for test

--- src/axios-options.js
+++ src/axios-options.js
@@ -11,14 +11,11 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   // Remove comments from CSRT_TOKEN variable and the X-XSRF-TOKEN header for production.
   // Also replace the 'baseURL' value with the correct value for the server backend (e.g. Heroku).
 
-  const CSRF_TOKEN = document.cookie.match(new RegExp('XSRF-TOKEN=([^;]+)'))[1];
-
   instance = axios.create({
     withCredentials: true,
     // baseURL: 'http://localhost:8080/api',
     baseURL: 'https://206.81.27.39/api',
     headers: { 
-      'X-XSRF-TOKEN': CSRF_TOKEN,
       'Access-Control-Allow-Methods': 'PATCH, DELETE, POST, GET, OPTIONS, PUT'
     }
   });
