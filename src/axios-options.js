041de6049f67b83a80ec6b46ae20532610b7118a
Tommi Lepola <lepola.tommi@gmail.com>
2019-08-22 18:09:27 +0300

    Prepare for deployment

--- src/axios-options.js
+++ src/axios-options.js
@@ -16,7 +16,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   instance = axios.create({
     withCredentials: true,
     // baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://vc-system.herokuapp.com/api',
+    baseURL: 'https://206.81.27.39/api',
     headers: { 
       'X-XSRF-TOKEN': CSRF_TOKEN,
       'Access-Control-Allow-Methods': 'PATCH, DELETE, POST, GET, OPTIONS, PUT'
