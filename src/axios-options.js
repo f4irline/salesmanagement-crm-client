8a56b173ce269213f159069f2e715e939b887d7a
Tommi Lepola <lepola.tommi@gmail.com>
2019-08-29 23:01:50 +0300

    Try without https

--- src/axios-options.js
+++ src/axios-options.js
@@ -16,7 +16,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   instance = axios.create({
     withCredentials: true,
     // baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://206.81.27.39/api',
+    baseURL: 'http://206.81.27.39/api',
     headers: { 
       'X-XSRF-TOKEN': '12345',
       'Access-Control-Allow-Methods': 'PATCH, DELETE, POST, GET, OPTIONS, PUT'
